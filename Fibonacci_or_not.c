84f71af30a7c37c078d6d2d1ba37a0af346a8ca4
ARJUNTUTIKA <119719352+ARJUNTUTIKA@users.noreply.github.com>
2022-12-23 18:34:46 +0530

    #include<stdio.h> int main() {     int n,a=0,b=1,c=0,i;     scanf("%d",&n);     for(i=1;c<n;i++)     {         c=a+b;         a=b;         b=c;     }     if(c==n)     {         printf("True");     }     else     {         printf("False");     } }

---
+++ Fibonacci_or_not.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int n,a=0,b=1,c=0,i;
+    scanf("%d",&n);
+    for(i=1;c<n;i++)
+    {
+        c=a+b;
+        a=b;
+        b=c;
+    }
+    if(c==n)
+    {
+        printf("True");
+    }
+    else
+    {
+        printf("False");
+    }
+}
