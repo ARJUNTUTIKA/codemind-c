8d3668a1a7f90585b8899f752312662bb2891bf5
ARJUNTUTIKA <119719352+ARJUNTUTIKA@users.noreply.github.com>
2022-12-25 10:55:25 +0530

    #include<stdio.h>
    int main(){
        
        int n,i,j;scanf("%d",&n);
        for(i=1;i<=n;i++){
            for(j=1;j<=n;j++){
                if(j==i || j==(n-i+1)) printf("x");
                else printf("0");
            }
            printf("
    ");
        }
    }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -2,15 +2,11 @@
 
 int main(){
     
-    int n,i,j;
-    scanf("%d",&n);
+    int n,i,j;scanf("%d",&n);
     for(i=1;i<=n;i++){
-        for(j=1;j<=i;j++){
-            if(i==1 || i==n) printf("*");
-            else{
-                if(j==1 || j==i) printf("*");
-                else printf(" ");
-            }
+        for(j=1;j<=n;j++){
+            if(j==i || j==(n-i+1)) printf("x");
+            else printf("0");
         }
         printf("
 ");
